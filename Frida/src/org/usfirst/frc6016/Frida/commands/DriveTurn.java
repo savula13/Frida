edf4d88e30de2227c47c5b69cb66c32792023c9e
LAPTOP-JR7P5S25\saiav <sai.avula13@gmail.com>
2018-03-02 16:49:14 -0500

    Test

---
+++ Frida/src/org/usfirst/frc6016/Frida/commands/DriveTurn.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc6016.Frida.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class DriveTurn extends CommandGroup {
+
+    public DriveTurn(double distance, double angle) {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    	SmartDashboard.putString("DriveTurn", "Run");
+    	addSequential(new DriveStraight(distance));
+    	addSequential(new Turn(angle));
+    }
+}
